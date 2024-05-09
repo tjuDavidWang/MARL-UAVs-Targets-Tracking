做出了以下调整 1. 修改了保存权重, 动画, 坐标等路径 2. 修改了计算pmi reward时没有归一化的bug, 以及pmi输出为负的bug(softmax added)

src/train.py
@@ -19,15 +19,16 @@ def train(config, env, agent, pmi, num_episodes, num_steps, frequency):
     :param num_episodes: 局数
     :return:
     """
+    # initialize saving list
     writer = SummaryWriter(log_dir=os.path.join(config["save_dir"], 'logs'))  # 可以指定log存储的目录
-
     return_list = []
     target_tracking_return_list = []
     boundary_punishment_return_list = []
     duplicate_tracking_punishment_return_list = []
+
     with tqdm(total=num_episodes, desc='Episodes') as pbar:
         for i in range(num_episodes):
-            # initial environment
+            # reset environment
             env.reset(t_v_max=config["target"]["v_max"],
                       t_h_max=pi / float(config["target"]["h_max"]),
                       u_v_max=config["uav"]["v_max"],
@@ -36,6 +37,8 @@ def train(config, env, agent, pmi, num_episodes, num_steps, frequency):
                       dc=config["uav"]["dc"],
                       dp=config["uav"]["dp"],
                       dt=config["uav"]["dt"],)
+
+            # reset return
             episode_return = 0
             episode_target_tracking_return = 0
             episode_boundary_punishment_return = 0
@@ -66,40 +69,50 @@ def train(config, env, agent, pmi, num_episodes, num_steps, frequency):
                 episode_boundary_punishment_return += sum(reward_list['boundary_punishment'])
                 episode_duplicate_tracking_punishment_return += sum(reward_list['duplicate_tracking_punishment'])
 
+            # saving return lists
             return_list.append(episode_return)
             target_tracking_return_list.append(episode_target_tracking_return)
             boundary_punishment_return_list.append(episode_boundary_punishment_return)
             duplicate_tracking_punishment_return_list.append(episode_duplicate_tracking_punishment_return)
 
+            # update actor-critic network
             actor_loss, critic_loss = agent.update(transition_dict)
             writer.add_scalar('actor_loss', actor_loss, i)
             writer.add_scalar('critic_loss', critic_loss, i)
 
+            # update pmi network
             if pmi:
                 avg_pmi_loss = pmi.train_pmi(config, torch.tensor(np.array(transition_dict["states"])), env.n_uav)
                 writer.add_scalar('avg_pmi_loss', avg_pmi_loss, i)
 
             if (i + 1) % frequency == 0:
                 # print some information
-                pbar.set_postfix({'episode': '%d' % (i + 1),
-                                  'return': '%.3f' % np.mean(return_list[-frequency:])})
-                print(f"actor loss: {actor_loss}, critic loss: {critic_loss}")
+                if pmi:
+                    pbar.set_postfix({'episode': '%d' % (i + 1),
+                                      'return': '%.3f' % np.mean(return_list[-frequency:]),
+                                      'actor loss': '%f' % actor_loss,
+                                      'critic loss': '%f' % critic_loss,
+                                      'avg pmi loss': '%f' % avg_pmi_loss})
+                else:
+                    pbar.set_postfix({'episode': '%d' % (i + 1),
+                                      'return': '%.3f' % np.mean(return_list[-frequency:]),
+                                      'actor loss': '%f' % actor_loss,
+                                      'critic loss': '%f' % critic_loss})
 
                 # save results and weights
                 draw_animation(config=config, env=env, num_steps=num_steps, ep_num=i)
                 agent.save(config["save_dir"], i + 1)
                 if pmi:
-                    print(f"avg pmi loss: {avg_pmi_loss}")
                     pmi.save(config["save_dir"], i + 1)
                 env.save_position(config["save_dir"], i + 1)
 
             pbar.update(1)
 
-            other_return_list = {
-                'target_tracking_return_list': target_tracking_return_list,
-                'boundary_punishment_return_list': boundary_punishment_return_list,
-                'duplicate_tracking_punishment_return_list': duplicate_tracking_punishment_return_list
-            }
+    other_return_list = {
+        'target_tracking_return_list': target_tracking_return_list,
+        'boundary_punishment_return_list': boundary_punishment_return_list,
+        'duplicate_tracking_punishment_return_list': duplicate_tracking_punishment_return_list
+    }
 
     writer.close()
 